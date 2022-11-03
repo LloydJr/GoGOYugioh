a4e9c3c9470ee0f1486dc2e5ca0b7c7c61406313
Lloyd Perez <lloyd4965jr@yahoo.com>
2022-11-03 00:35:07 -0400

    Updated about

--- src/Pages/About.js
+++ src/Pages/About.js
@@ -10,7 +10,10 @@ function About() {
         This is the about me page.
       </p>
       <p className="mb-3">
-      Hi my name is Lloyd Perez and I have been...
+     GogoYugioh is a web application that lets the user search through an index of all the known Yugioh cards and display the prices for the card if it is in near mint condition.
+     Yugioh is a card game in which two players attempt to defeat each other by decreasing their opponent's Life Points (down to 0) using a collection of monsters, spells, and trap cards.
+     The application will be using an API by YGOPRODeck to gather the necessary information for the user. The web application will be able to sort the card by selecting a user's choice of category and search for a specific card.
+     The project will be using Java, React.JS, MySQL, and Spring Boot.
       </p>
       </div>
   )
